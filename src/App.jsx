52d9a26f16f853f7bc8b8f139a5da31c2683bccf
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2025-01-24 22:32:56 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WheelSpinner from './components/WheelSpinner'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <WheelSpinner />
+    </div>
+  )
+}
+
+export default App
